9dfd9d4d17e8b986c9b17611dd646d427190f906
MayorFalomo <mayorfalomo@gmail.com>
2023-10-28 08:27:38 +0100

    completed the registration page

--- components/register/Register.tsx
+++ components/register/Register.tsx
@@ -1,31 +1,38 @@
 import Image from 'next/image'
 import React from 'react'
 import { MdOutlineArrowDropDown } from 'react-icons/md'
+import { FcGoogle} from 'react-icons/fc'
 
 type Props = {}
 
 const Register = (props: Props) => {
   return (
-      <div className='h-[100vh] flex justify-center items-center border-2 border-red-600' >
-          <div>
-              <Image src='./google.svg' width="100" height="100" alt='Google' />
-              <h1>Create a Hi-Notepad account </h1>
-              <button>Sign Up With Google</button>
-              <form>
-                  <input type="text" name="username" placeholder="Username" />
-                  <input type="email" name="email" placeholder="Email" />
-                  <input type="password" name="confirmPassword" placeholder="Enter Password" />
-              <button>Sign Up </button>
+      <div className='bg-white border-2 border-blue-600 text-black h-[100vh] flex justify-center items-center' >
+          <div className='flex flex-col justify-between items-center w-[650px] h-[95%]' >
+              <div  className='border-2 border-black-200 rounded-xl p-8 mt-2 flex flex-col justify-end mx-[auto] gap-6 w-[90%] ' >
+              <div  className='flex flex-col items-center gap-3' >
+                  <Image className='flex justify-center m-auto' src='./google.svg' width="120" height="120" alt='Google' />
+              <h1 className='text-[27px] text-center' >Create a Hi-Notepad account </h1>
+              <button className='w-[60%] text-[22px] border-2 border-black-500 rounded-[35px] flex justify-center gap-2 p-4' >{<FcGoogle size={30}  />} Sign Up With Google</button>
+              </div>
+                      <form className='flex flex-col items-end gap-4' >
+                  <input className='p-4 w-full rounded-[6px] bg-white border-2 border-black-500 placeholder:px-3' type="text" name="username" placeholder="Username" />
+                  <input  className='p-4 w-full  rounded-[6px] border-2 border-black-500 placeholder:px-3' type="email" name="email" placeholder="Email" />
+                  <input  className='p-4 w-full rounded-[6px] bg-white border-2 border-black-500 placeholder:px-3' type="password" name="confirmPassword" placeholder="Enter Password" />
+              <button className='border-2 border-blue-600 mt-6 p-3 w-1/4  bg-[#1B66C9] rounded-[8px] text-[18px] text-white' >Sign Up </button>
               </form>
-          </div>
-          <ul>
-              <li>English (United States)  <span>{<MdOutlineArrowDropDown />} </span> </li>
-              <div>
+                  </div>
+          <div className=' w-[90%]' >
+          <ul className='flex items-start justify-between w-full  ' >
+              <li className='flex items-center gap-8' >English (United States)  <span>{<MdOutlineArrowDropDown />} </span> </li>
+              <div className='flex items-center gap-4 ' >
                   <span>Help </span>
                   <span>Privacy </span>
                   <span>Terms </span>
+                  </div>
+              </ul>
               </div>
-          </ul>
+              </div>
     </div>
   )
 }
