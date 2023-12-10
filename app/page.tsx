19b4e05b7947ba0da559debf63d5bd9f340e0839
mayor falomo <mayorfalomo@gmail.com>
2023-12-10 06:53:30 +0100

    updated the Home.module.css

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Homepage from "./pages/home/page";
 import Head from "next/head";
 import Navbar from "@/components/navbar/Navbar";
 import Notes from "@/components/notes/Notes";
-import "./Home.module.css";
+import styles from "./Home.module.css";
 import Headerbar from "@/components/header/Headerbar";
 import ShowNotes from "@/components/showNotes/ShowNotes";
 import Pinned from "@/components/pinned/Pinned";
@@ -12,9 +12,9 @@ export default function Home() {
   return (
     <div>
       <Headerbar />
-      <div className="container">
+      <div className={styles.container}>
         <Navbar />
-        <div className="notesContain">
+        <div className={styles.notesContain}>
           <Notes />
           <div className="my-[50px]">
             <Pinned />
