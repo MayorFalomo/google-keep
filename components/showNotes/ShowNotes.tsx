f9025ef74c769441ea6be57092245e6ba2e6461f
mayor falomo <mayorfalomo@gmail.com>
2023-12-23 08:35:37 +0100

    added message to notes

--- components/showNotes/ShowNotes.tsx
+++ components/showNotes/ShowNotes.tsx
@@ -35,6 +35,7 @@ const ShowNotes = (props: any) => {
   const [showBgModal, setShowBgModal] = useState(false);
   const [successful, setSuccessful] = useState<boolean>(false);
   const [pinnedSuccess, setPinnedSuccess] = useState<boolean>(false);
+  const [emptyMessage, setEmptyMessage] = useState<boolean>(false);
 
   const router = useRouter();
 
@@ -42,16 +43,19 @@ const ShowNotes = (props: any) => {
     // if (userCookie) {
     axios
       .get(
-        `https://keep-backend-theta.vercel.app/api/notes/getall-notes/${contextValue?.user?._id}`
+        `https://keep-backend-theta.vercel.app/api/notes/getall-notes/${userCookie}`
       )
       .then((res) => contextValue?.setNotes(res.data.notes))
+      .then(() => setEmptyMessage(true))
       .catch((err) => console.log(err));
     // setPostLoaded(true);
     // } else {
     //   // router.push("/register");
     //   console.log("The id was not found");
     // }
-  }, []);
+  }, [userCookie]);
+
+  console.log(contextValue?.notes, "notes");
 
   const items = contextValue?.notes || []; // Assuming contextValue.notes is the array of items
 
@@ -254,71 +258,77 @@ const ShowNotes = (props: any) => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
         >
-          {contextValue?.notes?.length > 0 ? (
-            contextValue.notes?.map((note: any, index: any) => (
-              <div
-                // data-grid={{ ...note, x: currentX, y: currentY, w: 2, h: 2 }}
-                // data-grid={{ ...note, x: index }}
-                // data-grid={note}
-                onMouseEnter={() => {
-                  setShowIconsOnHover(true);
-                  setShowId(note?._id);
-                }}
-                onMouseLeave={() => {
-                  setShowIconsOnHover(false);
-                  setShowId("");
-                }}
-                className=" relative max-w-[350px] min-w-[250px] h-fit min-h-[120px] border-2 border-[#5F6368] mr-[25px] mb-[25px] rounded-[10px]"
-                style={{
-                  backgroundColor: note?.bgColor ? note?.bgColor : "#202124",
-                  backgroundImage: `url(${note?.bgImage})`,
-                  backgroundPosition: "center",
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat",
-                }}
-                key={note?._id}
-                id={contextValue?.note?._id}
-              >
-                {overLay ? (
-                  <AnimatePresence>
-                    <motion.div
-                      onClick={() => {
-                        setNoteModal(false);
-                        setOverLay(false);
-                        setShowBgModal(false);
-                      }}
-                      exit={{ opacity: 0 }}
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: 1 }}
-                      className="fixed z-10 top-0 left-0 h-full w-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 "
-                    ></motion.div>
-                  </AnimatePresence>
-                ) : (
-                  ""
-                )}
+          {emptyMessage ? (
+            contextValue?.notes?.length > 0 ? (
+              contextValue.notes?.map((note: any, index: any) => (
+                <div
+                  // data-grid={{ ...note, x: currentX, y: currentY, w: 2, h: 2 }}
+                  // data-grid={{ ...note, x: index }}
+                  // data-grid={note}
+                  onMouseEnter={() => {
+                    setShowIconsOnHover(true);
+                    setShowId(note?._id);
+                  }}
+                  onMouseLeave={() => {
+                    setShowIconsOnHover(false);
+                    setShowId("");
+                  }}
+                  className=" relative max-w-[350px] min-w-[250px] h-fit min-h-[120px] border-2 border-[#5F6368] mr-[25px] mb-[25px] rounded-[10px]"
+                  style={{
+                    backgroundColor: note?.bgColor ? note?.bgColor : "#202124",
+                    backgroundImage: `url(${note?.bgImage})`,
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                  key={note?._id}
+                  id={contextValue?.note?._id}
+                >
+                  {overLay ? (
+                    <AnimatePresence>
+                      <motion.div
+                        onClick={() => {
+                          setNoteModal(false);
+                          setOverLay(false);
+                          setShowBgModal(false);
+                        }}
+                        exit={{ opacity: 0 }}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        className="fixed z-10 top-0 left-0 h-full w-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 "
+                      ></motion.div>
+                    </AnimatePresence>
+                  ) : (
+                    ""
+                  )}
 
-                <ShowNote
-                  note={note}
-                  overLay={overLay}
-                  setOverLay={setOverLay}
-                  noteModal={noteModal}
-                  setNoteModal={setNoteModal}
-                  noteUrlParams={noteUrlParams}
-                  setNoteUrlParams={setNoteUrlParams}
-                  showIconsOnHover={showIconsOnHover}
-                  setShowIconsOnHover={setShowIconsOnHover}
-                  picture={picture}
-                  setPicture={setPicture}
-                  showId={showId}
-                  showBgModal={showBgModal}
-                  setShowBgModal={setShowBgModal}
-                  successful={successful}
-                  setSuccessful={setSuccessful}
-                  pinnedSuccess={pinnedSuccess}
-                  setPinnedSuccess={setPinnedSuccess}
-                />
+                  <ShowNote
+                    note={note}
+                    overLay={overLay}
+                    setOverLay={setOverLay}
+                    noteModal={noteModal}
+                    setNoteModal={setNoteModal}
+                    noteUrlParams={noteUrlParams}
+                    setNoteUrlParams={setNoteUrlParams}
+                    showIconsOnHover={showIconsOnHover}
+                    setShowIconsOnHover={setShowIconsOnHover}
+                    picture={picture}
+                    setPicture={setPicture}
+                    showId={showId}
+                    showBgModal={showBgModal}
+                    setShowBgModal={setShowBgModal}
+                    successful={successful}
+                    setSuccessful={setSuccessful}
+                    pinnedSuccess={pinnedSuccess}
+                    setPinnedSuccess={setPinnedSuccess}
+                  />
+                </div>
+              ))
+            ) : (
+              <div className="flex justify-center m-[auto] ">
+                <p className="text-[20px]"> You have no notes </p>
               </div>
-            ))
+            )
           ) : (
             <div className="flex justify-center m-[auto] ">
               <span className="loader"></span>
