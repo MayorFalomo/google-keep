688276c8c1def7727d80ffc7fc7a05c40fec22c8
mayor falomo <mayorfalomo@gmail.com>
2023-12-23 05:50:23 +0100

    setUser to null

--- helpers/ContextProvider.tsx
+++ helpers/ContextProvider.tsx
@@ -47,17 +47,16 @@ const AppContextProvider = ({ children }: any) => {
       `https://keep-backend-theta.vercel.app/api/users/get-user/${id}`
     );
     try {
-      if (res) {
+      if (res.data) {
         router.push("/");
         console.log(res.data);
+        setUser(res.data);
         toast.success("Login Successful!");
-
-        return setUser(res.data);
         // return res.data;
       } else {
         router.push("/register");
         console.log("The id was not found");
-        return null;
+        setUser(null);
       }
     } catch (error) {
       console.log(error && router.push("/register"));
